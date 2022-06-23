850e3ddf40c30abe0e837eb38e0b0b93f5274c2c
Gaurav <Gaurav@GAURAV>
2022-06-23 12:36:13 +0530

    Add project files.

---
+++ Shaurya/Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shaurya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
